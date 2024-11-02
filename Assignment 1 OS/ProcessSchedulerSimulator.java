438120d21051f2d3c318596af221069acc85272f
Sarahfhd <145019237+Sarahfhd@users.noreply.github.com>
2024-11-03 02:51:36 +0300

    Add files via upload

---
+++ Assignment 1 OS/ProcessSchedulerSimulator.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class ProcessSchedulerSimulator {
+    public static void main(String[] args) {
+        Scheduler scheduler = new Scheduler();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of processes: ");
+        int numProcesses = scanner.nextInt();
+
+        for (int i = 0; i < numProcesses; i++) {
+            System.out.print("Enter arrival time and burst time for Process " + (i + 1) + ": ");
+            int arrivalTime = scanner.nextInt();
+            int burstTime = scanner.nextInt();
+            scheduler.addProcess(new Process(i + 1, arrivalTime, burstTime));
+        }
+
+        scheduler.roundRobinScheduling();
+        scanner.close();
+    }
+}
+
